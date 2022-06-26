da684c152fc37c013bdfdf99765ac88f09a26904
MrGAFs <gogo172000@gmail.com>
2022-06-26 12:11:09 +0200

    .

--- api/src/handlers/Picture.ts
+++ api/src/handlers/Picture.ts
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
 		cb(null, './uploads');
 	},
 	filename: (req, file, cb) => {
-		cb(null, `${Date.now()}-${file.filename}.jpg`);
+		cb(null, `${req.body.user_id}${file.originalname}`);
 	}
 });
 
