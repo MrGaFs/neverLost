1b5809680db4913eeaea161fe786707e3f85d189
MrGAFs <gogo172000@gmail.com>
2022-06-26 12:25:52 +0200

    resloving the images problem

--- api/src/handlers/Picture.ts
+++ api/src/handlers/Picture.ts
@@ -1,6 +1,7 @@
 import Picture from '../models/Picture';
 import express from 'express';
 import multer from 'multer';
+import path from 'path';
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
@@ -19,7 +20,7 @@ const getPicture = async (req: express.Request, res: express.Response) => {
 		const ret = await pic.getPicture(parseInt(req.params.id as string));
 		if(!ret)
 			throw new Error('No picture');
-		res.status(200).sendFile(ret.path);
+		res.status(200).sendFile(path.resolve(ret.path));
 	} catch (e) {
 		res.status(500).send((e as Error).message);
 	}
@@ -30,7 +31,7 @@ const addPicture = async (req: express.Request, res: express.Response) => {
 		if (!req.file)
 			throw new Error('No file');
 		const ret = await pic.createPicture({
-			user_id: req.body.id,
+			user_id: parseInt(req.body.user_id),
 			path: req.file?.path as string
 		});
 		res.status(200).json(ret);
